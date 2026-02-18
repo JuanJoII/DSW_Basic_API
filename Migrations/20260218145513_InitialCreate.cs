f67de6554d7a9b09230d3ac8647afcfa198170ce
Juan José Camacho <juanjose34251@gmail.com>
2026-02-18 10:24:53 -0500

    Add initial migration files and update .gitignore

---
+++ Migrations/20260218145513_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web_API_Usuario.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "usuario",
+                columns: table => new
+                {
+                    id_usuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombre = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    apellido = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    correo = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    username = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true),
+                    fecha_creacion = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__usuario__4E3E04AD84577259", x => x.id_usuario);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "usuario");
+        }
+    }
+}
