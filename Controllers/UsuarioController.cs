31df3f41f3a06f982cfd37dbca10575e3606e3bb
JuanJoII <juanjose34251@gmail.com>
2026-02-18 23:17:44 -0500

    Refactor UsuarioController to improve response handling and add status codes for user retrieval and updates

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -23,7 +23,11 @@ namespace Web_API_Usuario.Controllers
         public async Task<ActionResult<IEnumerable<Usuario>>> ListarUsuario()
         {
             var usuarios = await _context.Usuario.ToListAsync();
-            return Ok(usuarios);
+            if (usuarios != null)
+            {
+                return Ok(StatusCode(StatusCodes.Status200OK, usuarios));
+            }
+            return NotFound(StatusCode(StatusCodes.Status404NotFound, "No hay usuarios registrados"));
         }
 
         [HttpPost("Guardar")]
@@ -41,9 +45,9 @@ namespace Web_API_Usuario.Controllers
             var usuario = await _context.Usuario.FindAsync(id);
             if (usuario == null)
             {
-                return NotFound();
+                return NotFound(StatusCode(StatusCodes.Status404NotFound, "Usuario no encontrado"));
             }
-            return Ok(usuario);
+            return Ok(StatusCode(StatusCodes.Status200OK, usuario));
         }
 
         [HttpDelete("Eliminar/{id}")]
@@ -52,11 +56,27 @@ namespace Web_API_Usuario.Controllers
             var usuario = await _context.Usuario.FindAsync(id);
             if (usuario == null)
             {
-                return NotFound();
+                return NotFound(StatusCode(StatusCodes.Status404NotFound, "Usuario no encontrado"));
             }
             _context.Usuario.Remove(usuario);
             await _context.SaveChangesAsync();
-            return NoContent();
+            return Ok(StatusCode(StatusCodes.Status200OK, usuario));
+        }
+
+        [HttpPut("Actualizar/{id}")]
+        public async Task<ActionResult> ActualizarUsuario(int id, Usuario usuarioActualizado)
+        {
+            var usuario = await _context.Usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound(StatusCode(StatusCodes.Status404NotFound, "Usuario no encontrado"));
+            }
+            usuario.Nombre = usuarioActualizado.Nombre;
+            usuario.Apellido = usuarioActualizado.Apellido;
+            usuario.Correo = usuarioActualizado.Correo;
+            usuario.Username = usuarioActualizado.Username;
+            await _context.SaveChangesAsync();
+            return Ok(StatusCode(StatusCodes.Status200OK, usuario));
         }
 
     }
